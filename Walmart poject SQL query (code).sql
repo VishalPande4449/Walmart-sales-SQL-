250edad2de945f4f490de6516d86e032eb513048
VishalPande4449 <105113023+VishalPande4449@users.noreply.github.com>
2024-03-17 05:01:51 +0530

    Walmart poject SQL query (code).sql

--- Walmart poject SQL query (code).sql
+++ Walmart poject SQL query (code).sql
@@ -39,10 +39,8 @@ FROM sales;
 
 ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);     -- create a new table
 
--- For this to work turn off safe mode for update
--- Edit > Preferences > SQL Edito > scroll down and toggle safe mode
--- Reconnect to MySQL: Query > Reconnect to server
-UPDATE sales              -- data will be filtered and put into newly made column
+
+UPDATE sales            
 SET time_of_day = (
 	CASE
 		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
@@ -53,28 +51,27 @@ SET time_of_day = (
 
 
 -- Day_name
--- Adding column name day_name for transactions happenend on a specific day (monday,tuesday etc) to understand which day a store performs best.
-SELECT date,        -- query to sort date data acc to day names
+SELECT date,        
     DAYNAME(date)
 FROM sales;
 
-ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);    -- creating a null table 
+ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);    
 UPDATE sales
-SET day_name = DAYNAME(date);    -- updating data in day_name columns
+SET day_name = DAYNAME(date);    
 
 
 
 -- month_name
--- The exact month in which the sale has happened, to determine which months have the most profit.
 
-SELECT date,        -- query to sort date data acc to month names
+
+SELECT date,        
     MONTHNAME(date)
 FROM sales;
 
-ALTER TABLE sales ADD COLUMN month_name VARCHAR(20);   -- creates new table
+ALTER TABLE sales ADD COLUMN month_name VARCHAR(20);   
 
 UPDATE sales
-SET month_name = MONTHNAME(date);         -- updating data in month_name columns
+SET month_name = MONTHNAME(date);         
 
 
 
@@ -84,12 +81,12 @@ SET month_name = MONTHNAME(date);         -- updating data in month_name columns
 
 -- How many unique cities does the data have?
 
-SELECT DISTINCT city     -- DISTINCT function sorts unique values
+SELECT DISTINCT city    
 FROM sales;
 
 
 -- How many branches each city has?
-SELECT DISTINCT city,    -- putting city, branch gives branch names according to cities 
+SELECT DISTINCT city,    
         branch    
 FROM sales;
 
@@ -98,32 +95,32 @@ FROM sales;
 
 -- how many unique product lines does the data have?
 SELECT 
-   COUNT(DISTINCT product_line)      -- count will actually count the values that are asked 
+   COUNT(DISTINCT product_line)      
 FROM sales;
 
 -- what is the most common payment method?
 SELECT payment,
-      COUNT(payment) AS count    -- counting payment values i.e. count of cash - 344, credit - 309 etc
+      COUNT(payment) AS count   
 FROM sales 
-GROUP BY payment;   -- this GROUP BY payment will give out the numbers accordingly with payment method cash, credit etc
+GROUP BY payment;   
 
 -- What is the most selling product line?
 SELECT product_line,
        COUNT(product_line) AS count 
 FROM sales
 GROUP BY product_line
-ORDER BY count DESC;     --  this will give highest number of counts in product line on top.
+ORDER BY count DESC;     
 
 
 -- What is the total revenue by month?(revenue in Jan, FEB, march)
 SELECT month_name AS month,       
-      SUM(total) AS total_revenue     -- Here SUM function is used to total all the revenue for 1 month, then next month.
+      SUM(total) AS total_revenue     
 FROM sales 
-GROUP BY month_name                   -- grouping is done by name of months ie Jan, Feb, March.
-ORDER BY total_revenue DESC;          -- the table is ordered according to total revenue in descending order
+GROUP BY month_name                  
+ORDER BY total_revenue DESC;          
 
 -- Which month has the largest cogs(cost of goods sold)
-SELECT month_name,                      -- Same as above, combo of cogs and month
+SELECT month_name,                      
       SUM(cogs) AS total_cogs   
 FROM sales 
 GROUP BY month_name                 
@@ -131,7 +128,7 @@ ORDER BY total_cogs DESC;
 
 -- Which product line has the largest revenue?
 SELECT product_line AS product,       
-      SUM(total) AS total_revenue     -- Same as above, just a combo of product line and total 
+      SUM(total) AS total_revenue    
 FROM sales 
 GROUP BY product_line                  
 ORDER BY total_revenue DESC;  
@@ -139,49 +136,48 @@ ORDER BY total_revenue DESC;
 
 -- What is the city with the largest revenue?
 SELECT city,      
-      SUM(total) AS total_revenue     -- Same as above, just a combo of city and total 
+      SUM(total) AS total_revenue    
 FROM sales 
 GROUP BY city                 
 ORDER BY total_revenue DESC;  
 
 -- What product line has the largest VAT?
 SELECT product_line AS product,       
-      SUM(tax_pct) AS VAT  -- Same as above, just a combo of product line and VAT(tax_pct)
+      SUM(tax_pct) AS VAT  
 FROM sales 
 GROUP BY product_line                  
 ORDER BY VAT DESC;  
 
 -- Fetch each product line and add a column of rating ie Good, Bad (Combo of product line and cogs but with a condition). Good if its gretaer than average sales, bad if it less than average sales.
 SELECT product_line,
-  CASE                                -- CASE WHEN ELSE just like if else 
-	  WHEN cogs > (SELECT AVG(cogs)   -- first condition 
+  CASE                                 
+	  WHEN cogs > (SELECT AVG(cogs)   
 	FROM sales) 
       THEN 'Good'                     
-	  ELSE 'Bad'                        -- 2nd condition ie cogs < average cogs 
-  END AS rating                       -- New column which will constitute good or bad rating
+	  ELSE 'Bad'                       
+  END AS rating                      
 FROM sales;
 
 -- Which branch sold more products than average products sold?
 SELECT branch,
-     SUM(quantity)  -- SUM of products sold(quantity)
+     SUM(quantity)  
 FROM sales 
 GROUP BY branch  
-HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);      -- HAVING is used to sort as this condition contains SUM and algorithm, after GROUP BYwe have to use 
-
+HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);     
 
 -- What is the most common product line by gender
 SELECT
-	gender,                                    -- 2 columns aree related in a table ie Maximum buying gender, product_line, total count(no. of male/female buyers of a product line)
+	gender,                                    
     product_line,
-    COUNT(gender) AS total_count               -- counting maximum buying gender for a product line
+    COUNT(gender) AS total_count               
 FROM sales 
-GROUP BY gender,product_line                   -- as both columns need to be showed and data is sported acc to both, we use both in here 
+GROUP BY gender,product_line                   
 ORDER BY total_count DESC;
 
 -- What is the average rating of each product line?
 
 SELECT 
-     AVG(rating) AS avg_rating,      -- AVG function is used to calculate average of ratings
+     AVG(rating) AS avg_rating,      
      product_line
 FROM sales
 GROUP BY product_line                
@@ -193,10 +189,10 @@ ORDER BY avg_rating DESC;
 
 -- Number of sales each time of the day every weekend
 SELECT 
-     time_of_day,                                      -- Each time of day = morning, afternoon, evening
-     COUNT(quantity) AS total_sales                    -- quanity = units sold in a transaction. and sorting it acc to units sold in a time of day
+     time_of_day,                                      
+     COUNT(quantity) AS total_sales                    
 FROM sales
-WHERE day_name = "SUNDAY"                               -- We have to sort acc to every day of the week, hence usinhg WHERE condition one by one for each day
+WHERE day_name = "SUNDAY"                               
 GROUP BY time_of_day
 ORDER BY total_sales DESC;
 
@@ -245,7 +241,7 @@ FROM sales;
 -- Which is the most common customer_type?
 
 SELECT customer_type,
-    COUNT(customer_type) AS customer               -- Here customer_type is Normal or member, But customer is total count of each one of these 2 entities.
+    COUNT(customer_type) AS customer              
 FROM sales
 GROUP BY customer_type
 ORDER BY customer DESC;
@@ -273,8 +269,8 @@ SELECT
      gender,                                    
      COUNT(gender) AS gender_cnt          
 FROM sales
-WHERE branch = "C"                               -- This C will change to A and B manually and we will get data accordingly
-GROUP BY gender                                -- NOTE - Here we can only mention the column acc to which we have to sort data, We nhave sort acc to gender here
+WHERE branch = "C"                              
+GROUP BY gender                               
 ORDER BY gender_cnt DESC;
 
 -- What time of day customers gives more ratings (number of rating)?
@@ -289,10 +285,10 @@ ORDER BY ratings DESC;
 -- What time of day customers gives more ratings (top ratings-5star,6star,7star)?
 SELECT 
      time_of_day,
-     AVG(rating) AS ratings        -- AVG will give out avg rating given by customer in a time of day
+     AVG(rating) AS ratings       
 FROM sales
 GROUP BY time_of_day               
-ORDER BY ratings DESC;             -- This AVG will be compared and maximum ratings(acc. to rating) in a specific time will be on top
+ORDER BY ratings DESC;             
 
 -- Which time of the day do customerrs give more rating( number of rating) acc to branches
 
@@ -300,7 +296,7 @@ SELECT
      time_of_day,
      COUNT(rating) AS ratings
 FROM sales 
-WHERE branch = "A"                           -- Here we have just added the condition which is branch and this is done by WHERE clause
+WHERE branch = "A"                           
 GROUP BY time_of_day
 ORDER BY ratings DESC;
 
